3cfc9f6966f75954d54f0659daf206c89d99e706
kimsehwan96 <sktpghks138@gmail.com>
2024-04-26 15:01:55 +0900

    Fix bugs with kube-apiserver commnad line arguments

--- certs/create.sh
+++ certs/create.sh
@@ -47,8 +47,8 @@ kubectl config set-context default --cluster=hard --user=system:kube-scheduler -
 kubectl config use-context default --kubeconfig=./scheduler.kubeconfig
 
 kubectl config set-cluster hard --certificate-authority=ca.crt --embed-certs=true --server=https://127.0.0.1:6443 --kubeconfig=./admin.kubeconfig
-kubectl config set-credentials admin --client-certificate=admin.crt --client-key=admin.key --embed-certs=true --kubeconfig=./admin.kubeconfig
-kubectl config set-context default --cluster=hard --user=admin --kubeconfig=./admin.kubeconfig
+kubectl config set-credentials kubernetes-admin --client-certificate=admin.crt --client-key=admin.key --embed-certs=true --kubeconfig=./admin.kubeconfig
+kubectl config set-context default --cluster=hard --user=kubernetes-admin --kubeconfig=./admin.kubeconfig
 kubectl config use-context default --kubeconfig=./admin.kubeconfig
 
 kubectl config set-cluster hard --certificate-authority=ca.crt --embed-certs=true --server=https://127.0.0.1:6443 --kubeconfig=./kubelet.kubeconfig
